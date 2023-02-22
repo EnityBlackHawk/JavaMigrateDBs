a3437d9208d40d63062cce6bb866d3f90ea31546
Luan Felipe <luanf2003@gmail.com>
2023-02-22 14:48:13 -0300

    Now setters can be autogenerated too

--- src/main/java/com/blackHawk/migrate/BaseClasses/OrderBaseClass.java
+++
@@ -1,2 +0,0 @@
-package com.blackHawk.migrate.BaseClasses;public class OrderBaseClass {
-}
--- src/main/java/com/blackHawk/migrate/BaseClasses/TesteInterface.java
+++ src/main/java/com/blackHawk/migrate/BaseClasses/TesteInterface.java
@@ -4,5 +4,6 @@ public interface TesteInterface {
 
     public int getId();
     public String getName();
+    public void setId(int id);
 
 }
